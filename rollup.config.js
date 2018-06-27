dc243bfb8748bf55328f62ed626a146513454f42
Arthur Langereis <puurtuur@me.com>
2018-06-27 12:59:08 +0200

    style: normalize build output

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,16 @@ export default [
 		input: "src/index.ts",
 		output: [
 			{
-				file: "dist/index.esm.js",
+				file: "dist/sd-streams.esm.js",
 				format: "es",
 				sourcemap: false,
+				compact: true,
+				banner: `/**
+* @stardazed/streams - implementation of the web streams standard
+* Part of Stardazed
+* (c) 2018 by Arthur Langereis - @zenmumbler
+* https://github.com/stardazed/sd-streams
+*/`
 			}
 		],
 		plugins: [
